770cd8682aa5528302296088c1ea6d26f45c34e0
DHLAP\h <vdavidhammond@gmail.com>
2017-08-07 23:36:15 -0600

    changed background color on both pages, restructure questions to be side by side, moved position of modal to more logical, cleaned up a few other small errors

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,8 @@
-var friendArray = [{name: "Mike", photo: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwicjJXHmcPVAhVChlQKHZIuARYQjRwIBw&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FMichael_Jordan&psig=AFQjCNHZuFmWTGXlVXtqW_dCwhN-iFQ40w&ust=1502129218641098", scores: [5, 2, 1, 4, 3, 1, 5, 4, 2, 2]},
-{name: "Crystal", photo: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwiVytC7mcPVAhXlzVQKHUKzCC8QjRwIBw&url=http%3A%2F%2Fcatsplat.deviantart.com%2Fart%2FCrystal-Headshot-672214104&psig=AFQjCNHIM264LPcmG7eNL03B8WTnuN653g&ust=1502129190888781", scores: [4, 2, 3, 5, 1, 4, 5, 2, 5, 2]},
-{name: "Mitch", photo: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwiN2qWumcPVAhVqj1QKHRWHDHgQjRwIBw&url=http%3A%2F%2Fwww.gossiprocks.com%2Fforum%2Fmiscellaneous%2F141752-david-hasselhoff-dons-his-red-baywatch-trunks-again.html&psig=AFQjCNHmRMtRwY230ygIcAqb1aOKDC9YOA&ust=1502129166427872", scores: [2, 5, 1, 1, 5, 1, 5, 1, 5, 5]},
+var friendArray = [{name: "Mike", photo: "https://upload.wikimedia.org/wikipedia/commons/a/ae/Michael_Jordan_in_2014.jpg", scores: [5, 2, 1, 4, 3, 1, 5, 4, 2, 2]},
+{name: "Crystal", photo: "https://photos1.blogger.com/blogger/5556/948/1600/Aughra1.gif", scores: [4, 2, 3, 5, 1, 4, 5, 2, 5, 2]},
+{name: "Mitch", photo: "https://vignette1.wikia.nocookie.net/baywatch/images/3/39/Mitch_1.jpg/revision/latest?cb=20151229155745", scores: [2, 5, 1, 1, 5, 1, 5, 1, 5, 5]},
 {name: "Olga", photo: "http://www.themakeupgallery.info/images/character/plain/fugly/dodge2.jpg", scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]},
-{name: "Hector", photo: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwjfiaHUmcPVAhXKiFQKHclCC5AQjRwIBw&url=http%3A%2F%2Fmoviecharactersinsouthpark.blogspot.com%2F&psig=AFQjCNHsNVcEGo3rsl7lBaW-AhxN_sUBQg&ust=1502129247039084", scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]}];
+{name: "Hector", photo: "https://vignette2.wikia.nocookie.net/disney/images/a/a2/Jack_Sparrow_Headshot.jpeg/revision/latest?cb=20151120173509", scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]},
+{name: "Cookie", photo: "https://s-media-cache-ak0.pinimg.com/736x/9d/de/ca/9ddeca43b2e588c525dc1b9f0ccf16cf--mona-lisa-smile-snooki.jpg", scores: [3, 2, 5, 3, 1, 5, 3, 2, 1, 1]}];
 
 module.exports = friendArray;
